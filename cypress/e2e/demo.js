308b7361b3b4482b48bb4b42bdd415380a500f47
JayAbueg-Rumor <jay.abueg@therumor.com>
2024-07-02 09:16:41 +0800

    Automation Test Initialize

---
+++ cypress/e2e/demo.js
@@ -0,0 +1,35 @@
+import '../support/commands'
+import 'cypress-mochawesome-reporter/register'
+const { expect } = require("chai")
+
+describe('Automation Assesment', () => {
+
+    it('Navigate to the demo home page', () => {
+        cy.visit('http://www.way2automation.com/demo.html#')
+    })
+
+
+    it('List action names of each category in json format', () => {
+
+    })
+
+    it('Retrive Traget url for the "Submit Button Clicked" event within the "Dynamic Elements" category', () => {
+
+    })
+
+    it('Visit the retrived link from Dynamic Elements category', () => {
+
+    })
+
+    it('Fill out the dummy registration', () => {
+
+    })
+
+    it(' Click the "EXPLORE LIFETIME MEMBERSHIP" link', () => {
+
+    })
+
+    it('Scroll to "30+ Courses video library FREE ACCESS"', () => {
+
+    })
+})
